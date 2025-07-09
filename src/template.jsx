e920211b4ce82eddd74f8fcd01a1fe082be5c381
unknown <ivo_08@abv.bg>
2025-07-09 10:55:01 +0300

    yup added

---
+++ src/template.jsx
@@ -0,0 +1,240 @@
+import React from 'react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+const validationSchema = yup.object({
+  firstName: yup.string().required('First name is required'),
+  lastName: yup.string().required('Last name is required'),
+  email: yup
+    .string('Enter your email')
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: yup
+    .string()
+    .min(8, 'Password must be at least 8 characters')
+    .required('Password is required'),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password'), null], 'Passwords must match')
+    .required('Confirm password is required'),
+  phone: yup
+    .string()
+    .matches(/^\d+$/, 'Phone number must contain only digits')
+    .required('Phone number is required'),
+  address1: yup.string().required('Address Line 1 is required'),
+  address2: yup.string(), // optional
+  city: yup.string().required('City is required'),
+  state: yup.string().required('State/Province is required'),
+  postalCode: yup
+    .string()
+    .matches(/^\d+$/, 'Postal Code must be numeric')
+    .required('Postal Code is required'),
+  country: yup.string().required('Country is required'),
+});
+
+const template = () => {
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      phone: '',
+      address1: '',
+      address2: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      country: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <Box sx={{ maxWidth: 600, mx: 'auto', p: 2 }}>
+      <Typography variant="h5" gutterBottom>
+        Registration Form
+      </Typography>
+
+      <form onSubmit={formik.handleSubmit}>
+        {/* Name Section */}
+        <TextField
+          fullWidth
+          margin="normal"
+          id="firstName"
+          name="firstName"
+          label="First Name"
+          value={formik.values.firstName}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.firstName && Boolean(formik.errors.firstName)}
+          helperText={formik.touched.firstName && formik.errors.firstName}
+        />
+
+        <TextField
+          fullWidth
+          margin="normal"
+          id="lastName"
+          name="lastName"
+          label="Last Name"
+          value={formik.values.lastName}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.lastName && Boolean(formik.errors.lastName)}
+          helperText={formik.touched.lastName && formik.errors.lastName}
+        />
+
+        {/* Account Info Section */}
+        <TextField
+          fullWidth
+          margin="normal"
+          id="email"
+          name="email"
+          label="Email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+
+        <TextField
+          fullWidth
+          margin="normal"
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+        />
+
+        <TextField
+          fullWidth
+          margin="normal"
+          id="confirmPassword"
+          name="confirmPassword"
+          label="Confirm Password"
+          type="password"
+          value={formik.values.confirmPassword}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+          helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
+        />
+
+        <TextField
+          fullWidth
+          margin="normal"
+          id="phone"
+          name="phone"
+          label="Phone Number"
+          value={formik.values.phone}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.phone && Boolean(formik.errors.phone)}
+          helperText={formik.touched.phone && formik.errors.phone}
+        />
+
+        {/* Address Section */}
+        <TextField
+          fullWidth
+          margin="normal"
+          id="address1"
+          name="address1"
+          label="Address Line 1"
+          value={formik.values.address1}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.address1 && Boolean(formik.errors.address1)}
+          helperText={formik.touched.address1 && formik.errors.address1}
+        />
+
+        <TextField
+          fullWidth
+          margin="normal"
+          id="address2"
+          name="address2"
+          label="Address Line 2 (Optional)"
+          value={formik.values.address2}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+
+        <TextField
+          fullWidth
+          margin="normal"
+          id="city"
+          name="city"
+          label="City"
+          value={formik.values.city}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.city && Boolean(formik.errors.city)}
+          helperText={formik.touched.city && formik.errors.city}
+        />
+
+        <TextField
+          fullWidth
+          margin="normal"
+          id="state"
+          name="state"
+          label="State / Province"
+          value={formik.values.state}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.state && Boolean(formik.errors.state)}
+          helperText={formik.touched.state && formik.errors.state}
+        />
+
+        <TextField
+          fullWidth
+          margin="normal"
+          id="postalCode"
+          name="postalCode"
+          label="Postal Code"
+          value={formik.values.postalCode}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.postalCode && Boolean(formik.errors.postalCode)}
+          helperText={formik.touched.postalCode && formik.errors.postalCode}
+        />
+
+        <TextField
+          fullWidth
+          margin="normal"
+          id="country"
+          name="country"
+          label="Country"
+          value={formik.values.country}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.country && Boolean(formik.errors.country)}
+          helperText={formik.touched.country && formik.errors.country}
+        />
+
+        <Button
+          color="primary"
+          variant="contained"
+          fullWidth
+          type="submit"
+          sx={{ mt: 2 }}
+        >
+          Submit
+        </Button>
+      </form>
+    </Box>
+  );
+};
+export default template;
