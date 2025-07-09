870ac210074a282b9fd99a5d0ba8a216bc3996c4
unknown <ivo_08@abv.bg>
2025-07-09 10:33:04 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import template from './template';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <template />
+    </div>
+  );
+}
+
+export default App; 
